acc653a1a74889e4e459ccf1ebfc9e99d16d3ad3
sunshinewyin <sunshine.w.yin@gmail.com>
2015-04-15 17:30:19 -0700

    Refactored to create function for sending responses

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,6 +3,17 @@ var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
 // require more modules/folders here!
 
+var sendResponse = function(response, statusCode, data, contentType) {
+  contentType = contentType || {'Content-Type': 'text/html'};
+  data = data || '';
+
+  response.writeHead(statusCode, contentType);
+  response.write(data);
+  response.end();
+};
+
+
+
 exports.handleRequest = function (request, response) {
 
   if (request.method === 'GET') {
@@ -10,9 +21,7 @@ exports.handleRequest = function (request, response) {
     // res.end(archive.paths.list);
       fs.readFile('web/public/index.html', 'utf-8', function(err,data){
         if (!err){
-          response.writeHead(200, {'Content-Type': 'text/html'});
-          response.write(data);
-          response.end();
+          sendResponse(response, 200, data);
         }else{
           console.log('ERROR FOR GET /',err,'+++');
         }
@@ -29,12 +38,9 @@ exports.handleRequest = function (request, response) {
       }
       fs.readFile(filePath, 'utf-8', function(err,data){
         if (!err){
-          response.writeHead(200, contentType);
-          response.write(data);
-          response.end();
+          sendResponse(response, 200, data, contentType);
         }else{
-          response.writeHead(404);
-          response.end();
+          sendResponse(response, 404);
         }
       });
     }
