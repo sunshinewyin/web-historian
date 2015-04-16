c7cbbf70b6c0fac8a100e53c1a78982fcd0eadbc
sunshinewyin <sunshine.w.yin@gmail.com>
2015-04-16 10:57:53 -0700

    Html fetcher creates array of URLs, downloads them

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,23 +1,44 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
 
-var archiveHelper = require('../helpers/archive-helpers');
-var http = require("http");
-var content = "";
+var fs = require('fs');
+var http = require('http');
+var archiveHelper = require('helpers/archive-helpers');
 
-var req = http.request('http://127.0.0.1:8080/loading.html', function(res) {
-  res.setEncoding("utf8");
-  res.on("data", function (chunk) {
+var downloadFile = function(fileName) {
+  var content = '';
+  var req = http.request('http://' + fileName, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
       content += chunk;
+    });
+    res.on('end', function () {
+      fs.writeFile('archives/sites/' + fileName, content, function(err){
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
   });
-  res.on("end", function () {
-      console.log(content);
+
+  req.on('error', function(e) {
+
   });
-});
 
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
+  req.end();
+};
 
-req.end();
+var handleUrls = function (arrOfUrls) {
+  fs.writeFile('archives/sites.txt', '', function(err){
+    if (err) {
+      console.log(err);
+    }
+  });
+  arrOfUrls.forEach(downloadFile);
+};
+
+archiveHelper.readListOfUrls(handleUrls);
+
+
+
 
